ab97422e2d44a23d2683d0e9e06828fb97708c2b
leechangho <ref275@naver.com>
2022-12-26 10:37:44 +0900

    first commit

---
+++ server/index.js
@@ -0,0 +1,35 @@
+// 익스프레스 서버 만들기
+const express = require('express');
+const app = express();
+const http = require('http');
+const {
+  Server
+} = require('socket.io');
+const cors = require('cors');
+
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+  });
+
+  socket.on('send_message', (data) => {
+    socket.to(data.room).emit("receive_message", data)
+  });
+});
+
+server.listen(3001, () => {
+  console.log("SERVER IS RUNNING");
+  console.log("9999999999")
+})
